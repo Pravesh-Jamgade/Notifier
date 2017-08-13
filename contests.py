3c388e2a52000474a6f74edc8e80d3d36577bf17
Saptarshi Dey <07saptarshidey@gmail.com>
2017-08-13 23:25:06 +0530

    Update1

---
+++ contests.py
@@ -0,0 +1,21 @@
+import bs4
+import time
+from urllib.request import urlopen as ureq
+from bs4 import BeautifulSoup as soup
+import subprocess as s
+
+myurl='https://www.codechef.com/contests'
+
+uclient=ureq(myurl)
+page_html=uclient.read();
+uclient.close()
+
+ps=soup(page_html,"html.parser")
+c=ps.findAll("tbody")
+container=c[0].findAll("tr")
+
+
+for i in container:
+	k=i.findAll("td")
+	s.call(['notify-send',"Contest Name: "+str(k[1].text),"Contest Duration: " + str(k[2].text) + " - " + str(k[3].text)])
+	#time.sleep(2)
