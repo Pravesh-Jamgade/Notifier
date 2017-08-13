d7e2e8452eed06edb99aa334b96a17bbf31e22bc
Saptarshi Dey <07saptarshidey@gmail.com>
2017-08-14 05:03:29 +0530

    Update2

--- contests.py
+++ contests.py
@@ -1,5 +1,4 @@
 import bs4
-import time
 from urllib.request import urlopen as ureq
 from bs4 import BeautifulSoup as soup
 import subprocess as s
@@ -12,10 +11,10 @@ uclient.close()
 
 ps=soup(page_html,"html.parser")
 c=ps.findAll("tbody")
-container=c[0].findAll("tr")
+container=c[1].findAll("tr")
 
+s.call(['notify-send',"Upcoming Contests:\n"])
 
 for i in container:
 	k=i.findAll("td")
 	s.call(['notify-send',"Contest Name: "+str(k[1].text),"Contest Duration: " + str(k[2].text) + " - " + str(k[3].text)])
-	#time.sleep(2)
